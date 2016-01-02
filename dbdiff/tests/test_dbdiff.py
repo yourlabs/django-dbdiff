cb6c4dfd71b2f932384bd412f7a6a3426e71684f
jpic <jamespic@gmail.com>
2016-01-02 11:20:22 +0100

    Dump predictibility: decimal normalization

--- dbdiff/tests/test_dbdiff.py
+++ dbdiff/tests/test_dbdiff.py
@@ -2,6 +2,8 @@
 
 from __future__ import unicode_literals
 
+import os
+
 from django import test
 from django.apps import apps
 from django.conf import settings
@@ -10,6 +12,7 @@ import six
 
 from . import base
 from .. import dbdiff
+from .project.decimal_test.models import TestModel as DecimalTestModel
 
 
 class DiffTest(test.TestCase):
@@ -30,3 +33,26 @@ class DiffTest(test.TestCase):
 +        "username": "test"
      },
 ''').lstrip()
+
+
+class DecimalDiffTest(test.TestCase):
+    fixtures = ['decimal_test_fixture']
+    expected = os.path.join(
+        'dbdiff',
+        'tests',
+        'project',
+        'decimal_test',
+        'fixtures',
+        'decimal_test_expected.json'
+    )
+    model = DecimalTestModel
+
+    def test_data_diff_is_empty(self):
+        assert not dbdiff.diff(self.expected)
+
+    def test_data_diff_has_changed(self):
+        model = self.model.objects.first()
+        model.test_field = '3.100'
+        model.save()
+
+        assert not dbdiff.diff(self.expected.replace('_expected', '_changed'))
