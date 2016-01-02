39971e8a6805c9ec6914dc0823efa0d204b8e985
jpic <jamespic@gmail.com>
2016-01-02 19:26:23 +0100

    Bump 0.2.0 [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='django-dbdiff',
-    version='0.1.0',
+    version='0.2.0',
     description='Database data diffing against fixtures for testing',
     author='James Pic',
     author_email='jamespic@gmail.com',
