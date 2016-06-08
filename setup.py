7c67edfaca4695b1413de80ad942352b28267146
jpic <jamespic@gmail.com>
2016-06-08 23:11:27 +0200

    Release 0.7.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='django-dbdiff',
-    version='0.6.0',
+    version='0.7.0',
     description='Database data diffing against fixtures for testing',
     author='James Pic',
     author_email='jamespic@gmail.com',
