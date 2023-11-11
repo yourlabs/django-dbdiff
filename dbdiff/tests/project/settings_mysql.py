42b1cc24ae5e06089d3ecbe1d9dcad0c3541cc73
Pascal F <pfouque@users.noreply.github.com>
2023-11-11 18:09:13 +0100

    Add github action

--- dbdiff/tests/project/settings_mysql.py
+++ dbdiff/tests/project/settings_mysql.py
@@ -1,9 +1,21 @@
 from .settings import *  # noqa
 
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dbdiff_test',
-        'USER': 'root',
+        'HOST': os.environ.get('DB_HOST', ''),
+        'NAME': os.environ.get('DB_NAME', 'dbdiff_test'),
+        'USER': os.environ.get('DB_USER', 'root'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'PORT': os.environ.get('DB_PORT', ''),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
+        'TEST':{
+            'CHARSET': 'utf8mb4',
+            'COLLATION': 'utf8mb4_unicode_ci',
+        }
     }
 }
--- dbdiff/tests/project/settings_postgresql.py
+++ dbdiff/tests/project/settings_postgresql.py
@@ -1,9 +1,16 @@
 from .settings import *  # noqa
 
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbdiff_test',
-        'USER': 'postgres',
+        'HOST': os.environ.get('DB_HOST', ''),
+        'NAME': os.environ.get('DB_NAME', 'dbdiff_test'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+        'OPTIONS': {},
+        
     }
 }
